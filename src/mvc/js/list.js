a0fe77d6dc7afd09fe54c4f0eed12f54717227e1
nabab <thomas.nabet@gmail.com>
2017-06-05 05:37:02 +0200

    Adaptation to component system

--- src/mvc/js/list.js
+++ src/mvc/js/list.js
@@ -1,35 +1,39 @@
-// Javascript Document
-$(".bbn-notes-list", ele).kendoGrid({
-  dataSource: {
-    serverPaging: true,
-    transport: {
-      read: {
-        url: data.root + "list",
-        type: "post"
+(function(){
+  return function(ele, data){
+    // Javascript Document
+    $(".bbn-notes-list", ele).kendoGrid({
+      dataSource: {
+        serverPaging: true,
+        transport: {
+          read: {
+            url: data.root + "list",
+            type: "post"
+          }
+        },
+        schema: {
+          data: "data",
+          total: "total"
+        },
+      },
+      columns: [{
+        field: "title",
+        title: "Titre",
+        width: 300
+      }, {
+        field: "content",
+        title: "Texte",
+        encoded: false
+      }, {
+        field: "medias",
+        title: "Medias",
+        width: 100
+      }],
+      sortable: true,
+      scrollable: true,
+      pageable: {
+        refresh: true,
+        pageSize: 100
       }
-    },
-    schema: {
-      data: "data",
-      total: "total"
-    },
-  },
-  columns: [{
-    field: "title",
-    title: "Titre",
-    width: 300
-  }, {
-    field: "content",
-    title: "Texte",
-    encoded: false
-  }, {
-    field: "medias",
-    title: "Medias",
-    width: 100
-  }],
-  sortable: true,
-  scrollable: true,
-  pageable: {
-    refresh: true,
-    pageSize: 100
+    });
   }
-});
+})();
